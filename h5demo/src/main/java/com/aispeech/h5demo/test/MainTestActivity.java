9b8c24ff6b2aa30993a5329ba4271ac700d0a82c
jiankai.deng <jiankai.deng@aispeech.com>
2019-11-11 10:25:26 +0800

    添加声纹测试用例

--- h5demo/src/main/java/com/aispeech/h5demo/test/MainTestActivity.java
+++ h5demo/src/main/java/com/aispeech/h5demo/test/MainTestActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import com.aispeech.h5demo.test.agent.AgentTestActivity;
 import com.aispeech.h5demo.test.tts.TTSTestActivity;
 import com.aispeech.h5demo.test.vad.VadTestActivity;
+import com.aispeech.h5demo.test.vprint.VprintTestActivity;
 import com.aispeech.h5demo.test.wakeup.WakeupTestActivity;
 
 
@@ -16,6 +17,7 @@ public class MainTestActivity extends BaseTestActivity {
         mData.add(new ListItem<>("TTS测试", TTSTestActivity.class));
         mData.add(new ListItem<>("vad测试", VadTestActivity.class));
         mData.add(new ListItem<>("云端对话测试", AgentTestActivity.class));
+        mData.add(new ListItem<>("声纹测试", VprintTestActivity.class));
         mData.add(new ListItem<>(" ", AgentTestActivity.class));
         mData.add(new ListItem<>("Agent 测试", AgentTestActivity.class));
         mData.add(new ListItem<>("场景模式测试", AgentTestActivity.class));
